b3d3066c4165a5b92da10cdfd7eb0a28a64f7987
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-03-26 23:36:56 +0530

    OS package

---
+++ prac.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+		 "fmt"
+		 "os"
+
+)
+
+func main(){
+		 fmt.Println("Using the os package and defining the Exit function. ")
+		 fmt.Println("Exit status is :- ")
+	   os.Exit(300)	
+}
