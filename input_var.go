8407c67c3a5476578dc363033b671247361ee842
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-02 23:58:30 +0530

    input variable

---
+++ input_var.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main()  {
+		fmt.Print("Enter number : ")
+		var input float64
+		fmt.Scanf("%f", &input)
+
+		output := input * 2
+		fmt.Println(output)
+}
+// func test() {
+// 		x := 5
+// 		fmt.Println(x)
+// 		x += 1
+// 		fmt.Println(x)
+// }
+
