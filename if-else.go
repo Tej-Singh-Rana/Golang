ad1ffda2708128af3d1546861cdcd3afc944ab1d
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-05 01:37:50 +0530

    statement added

--- if-else.go
+++ if-else.go
@@ -4,6 +4,10 @@ import "fmt"
 
 func main()  {
 	code := 2 
+	// an "if" statement, it has a condition followed by a block. if the condition evaluates to true then it run block conditions.
+	// an "if" statement is false then it will move to next "else if" or "else" condition if not present then it exit from block.
+	// condition checked followed by top down.
+	// the first one condition is true then it will not execute other blocks, even if their conditions also pass/true.
 	if  code <= 5 {
 			fmt.Println("This is less then or equal to 5")
 	} else if code == 10 {
