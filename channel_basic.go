5cdcc81f7e6307d68e4b549b045ca177d5516c20
Tej-Singh-Rana <iamtejsinghrana@gmail.com>
2020-04-15 19:08:31 +0530

    channel basics & examples

---
+++ channel_basic.go
@@ -0,0 +1,13 @@
+// goroutines need to communicate and they do safely via channels. goroutine --- channel --- goroutine
+// unbuffered & buffered
+// unbuffered channel --> channel-name := make(chan int) no size initialize.
+// buffered channel --> channel-name := make(chan int, 5) size available.
+
+package main
+
+import "fmt"
+
+func main(){
+				
+
+}
