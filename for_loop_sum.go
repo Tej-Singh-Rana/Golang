4a14d5bf4ed7de84f85daa076731783b60bb7cab
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2020-04-26 23:43:38 +0530

    define "for loop" in different format

---
+++ for_loop_sum.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+
+func main() {
+
+      sum := 1      // declared variable
+      // comparison 
+      for sum < 500 { 
+          sum += sum    // initialized condition
+       }
+      // o/p of sum of 2's combination
+      fmt.Println(sum)
+}
