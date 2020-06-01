3891d9ca6982c4c2307222f359c5a1f5b6979205
tej-singh-rana <iamtejsinghrana@gmail.com>
2020-06-01 23:53:30 +0530

    no-message

---
+++ stack.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+type Number struct{
+	c1 string
+	c2 int
+}
+
+func (n Number)(string){
+
+	c := n.c1("Hello")
+	return c
+}
+func main(){
+	fmt.Println(func(n))
+	
+}
+
