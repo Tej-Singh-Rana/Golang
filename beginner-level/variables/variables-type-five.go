25473bf0f719b257440d01e795bdd7b547d68d5b
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-02-16 23:18:04 +0530

    Short Variable Declaration

--- beginner-level/variables/variables-type-five.go
+++ beginner-level/variables/variables-type-five.go
@@ -1,12 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	// Declare variable
-	var 
-   // main block
-  fmt.Print()
+	// By using the short variable declaration operator (foo := "bar" --> string, foo := 5 --> int)
+	firstName := "Roni"
+	lastName := "Walker"
+	roomNumber := 45
 
-
+	fmt.Println("Name is", firstName, lastName, "and room number is", roomNumber, ".")
 }
