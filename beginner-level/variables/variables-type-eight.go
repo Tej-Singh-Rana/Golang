072d6aec431956656556ff81e8718f09e7e1c521
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-02-18 23:26:29 +0530

    Declare and Initialize in one line and Naming convention

---
+++ beginner-level/variables/variables-type-eight.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	// Declare and initialize in single line
+	/* Naming convention (Not recommended format):
+	first name  X  
+	45room      X
+	clOudTecH   X
+	jojo-code   X
+	$number     X
+	*/
+	nameStyle, formatOk, nameDealing := "likeThis", "rightOne", "number45"
+
+	print("Name style = ", nameStyle, ". Format could be = ", formatOk, ". Writing numeric in name = ", nameDealing, ".")
+}
