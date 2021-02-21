51c1da6df7d96ff708c555c5dbcb9c1262c19d2e
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-02-21 22:49:46 +0530

    Declare and Initialize in one line and Naming convention

--- beginner-level/variables/variables-type-eight.go
+++ beginner-level/variables/variables-type-eight.go
@@ -8,7 +8,9 @@ func main() {
 	clOudTecH   X
 	jojo-code   X
 	$number     X
+	Avoid giving a name from one of Go keywords such as package, if, break, switch, func and so on.. 
 	*/
+	
 	nameStyle, formatOk, nameDealing := "likeThis", "rightOne", "number45"
 
 	print("Name style = ", nameStyle, ". Format could be = ", formatOk, ". Writing numeric in name = ", nameDealing, ".")
