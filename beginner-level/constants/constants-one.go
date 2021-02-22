07032c4d80043f5a701020c41491e8486f22449a
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-02-23 00:08:29 +0530

    Keyword const

---
+++ beginner-level/constants/constants-one.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+/* We use "const" keyword for constants. Ex:- const Key = "900"
+Constants cannot be declared using the ":=" syntax. Like const Jago := "Alert"
+We can use boolean, numeric, string and character in place of values.
+*/
+
+func main() {}
