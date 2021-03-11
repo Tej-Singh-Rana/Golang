c9e8b41742e71a3622678337e2986c69b213c5b5
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-03-11 22:54:59 +0530

    Multiple short-hand var declaration

---
+++ beginner-level/variables/variables-type-eleven.go
@@ -0,0 +1,12 @@
+package main // package declaration
+
+import "fmt" // import "fmt" package
+
+// func body
+func main() {
+
+	// multiple short-hand var declaration
+	x, y, z := 4500, "plus", 45.56
+
+	fmt.Println(x, y, z)
+}
