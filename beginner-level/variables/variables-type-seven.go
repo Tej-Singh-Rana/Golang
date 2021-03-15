eacd92beb68a358351e39d1cf602edb74afb8ec2
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-03-16 00:42:28 +0530

    type conversion -> int to "string", "string" to int

--- beginner-level/variables/variables-type-seven.go
+++ beginner-level/variables/variables-type-seven.go
@@ -6,12 +6,20 @@ import (
 )
 
 var core int
+var studio string
 
 func main() {
-    // Specify type and perform a conversion
-    
+	// Specify type and perform a conversion
+	// Conversion int to "string"
 	core = 12
 	core1 := strconv.Itoa(core)
 	fmt.Printf("core1 value = %s and core1 type = %T", core1, core1)
+	fmt.Println("")
 
+	// Conversion "string" to int
+	studio = "50"
+	if studioCart, err := strconv.Atoi(studio); err == nil {
+		// %v -> print the value in a default format, %T -> print the type of the value.
+		fmt.Printf("studioCart value = %v and studioCart type = %T", studioCart, studioCart)
+	}
 }
