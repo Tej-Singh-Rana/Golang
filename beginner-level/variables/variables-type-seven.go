2cd679517ef0c224d3483dd2bf9f6f36f31ee69b
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-03-15 23:51:49 +0530

    Update variables-type-seven.go

--- beginner-level/variables/variables-type-seven.go
+++ beginner-level/variables/variables-type-seven.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-    "fmt"
-  )
+	"fmt"
+	"strconv"
+)
 
-  func main() {
+var core int
+
+func main() {
     // Specify type and perform a conversion
-      
-  }
+    
+	core = 12
+	core1 := strconv.Itoa(core)
+	fmt.Printf("core1 value = %s and core1 type = %T", core1, core1)
+
+}
