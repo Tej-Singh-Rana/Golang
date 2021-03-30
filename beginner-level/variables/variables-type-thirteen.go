93fe506cabd8f6e57930c7745f6e1a385eb0f667
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-03-30 23:52:56 +0530

    Variable Declaration and Initialization

---
+++ beginner-level/variables/variables-type-thirteen.go
@@ -0,0 +1,10 @@
+package main
+
+// variable declaration and assigned the value
+var msg string = "welcome to this"
+var tool string = msg + " area."
+
+// main func body
+func main() {
+	print(tool)
+}
