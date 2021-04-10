7e83de5f7e1580addd62043208213be5b8de9e10
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-11 00:05:18 +0530

    Update constants-three.go

--- beginner-level/constants/constants-three.go
+++ beginner-level/constants/constants-three.go
@@ -1,1 +1,16 @@
+package main
 
+import "fmt"
+
+// Declare multiple constant variables
+const (
+	roleOne   string = "Dev"
+	roleTwo   string = "Admin"
+	roleThree string = "Ops"
+)
+
+func main() {
+	// Declare inside main func
+	fmt.Println("Roles", "\n", roleOne, "\n", roleTwo, "\n", roleThree)
+
+}
