d2797a5f7d9bb059cbc2f680d85076e3de2d65ce
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-15 23:57:09 +0530

    Create fmt-three.go

---
+++ beginner-level/fmt/fmt-three.go
@@ -0,0 +1,11 @@
+package main
+
+// Provide a shorter alternative name to a library's package name
+
+import (
+	shortName "fmt"
+)
+
+func main() {
+	shortName.Println("It's a short alias name. Easy and effective.")
+}
