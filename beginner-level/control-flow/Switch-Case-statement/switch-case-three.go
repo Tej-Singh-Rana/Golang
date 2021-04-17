c2c4143faefa9ed60112b7abe4ed565b9ed42dab
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-17 23:50:41 +0530

    Simple case of "switch" statement

---
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-three.go
@@ -0,0 +1,16 @@
+package main  
+
+import "fmt"
+
+func main() {
+	// code block
+	goo := 10
+	switch goo {
+	case 10:
+		fmt.Println("Correct case.")
+	case 20:
+		fmt.Println("Incorrect case.")
+	default:
+		fmt.Println("None")
+	}
+}
