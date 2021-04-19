573a2a94366a8cfc005fb49b05b52c1051a3196c
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-19 23:54:01 +0530

    "For" loop condition and scope of variables

--- beginner-level/control-flow/For-loop/for-loop-two.go
+++ beginner-level/control-flow/For-loop/for-loop-two.go
@@ -1,1 +1,22 @@
+package main // main package declaration
 
+// Import package "fmt"
+import (
+	"fmt"
+)
+
+func main() {
+	var s int
+	for i := 1; i < 10; i++ {
+
+		// variable "i" can be used within a brackets. Outside of the brackets it can't be used.
+		s += s + i
+	}
+
+	// "fmt" package has Println() function.
+	fmt.Println(s)
+
+	// Trying to get a value of var "i"
+	// ERROR - undefined i
+	// fmt.Println(i)    // <-- uncomment that code, you will get an error as described above.
+}
