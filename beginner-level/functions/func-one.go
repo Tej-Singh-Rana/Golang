fe0d3c3d16a214425685e2b007d8bfa72836594b
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-21 23:34:11 +0530

    first function

---
+++ beginner-level/functions/func-one.go
@@ -0,0 +1,14 @@
+package main
+
+// Imported "fmt" package
+
+import (
+	"fmt"
+)
+
+// Main function - Main entrance
+
+func main() {
+	fmt.Println("This \"main\" function will execute our main source code.")
+}
+
