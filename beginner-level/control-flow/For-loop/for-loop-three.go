a422426b63890d2cda832f11e87302792db2d731
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-22 23:44:31 +0530

    Update for-loop-three.go

--- beginner-level/control-flow/For-loop/for-loop-three.go
+++ beginner-level/control-flow/For-loop/for-loop-three.go
@@ -1,1 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Slice + For Loop
+
+	cold := []int{1, 2, 3, 4, 5}
+
+	for index, value := range cold {
+
+		fmt.Println("Print the index = ", index, "| Print the element = ", value)
+	}
+}
 
