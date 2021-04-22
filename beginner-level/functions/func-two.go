ef9f0f7f2cd1ecf4edc18e2808b3d745c391accb
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-22 23:02:19 +0530

    Create func-two.go

---
+++ beginner-level/functions/func-two.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Multiple function declaration
+
+func summerNation() string {
+
+	// returning a string value and no argument passed
+
+	return "Too hot"
+}
+
+func coldNation(wait string) string {
+
+	return wait
+}
+
+func main() {
+
+	// Initialize a value with short declaration
+
+	vole := summerNation()
+
+	fmt.Println("What?", vole)
+
+	cole := coldNation("It's late")
+
+	fmt.Println(cole)
+
+}
