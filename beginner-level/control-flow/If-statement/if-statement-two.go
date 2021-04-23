c22ea310d130d8cf907b561f791acd1ae1f89553
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-23 23:48:03 +0530

    If-Else-If statement

--- beginner-level/control-flow/If-statement/if-statement-two.go
+++ beginner-level/control-flow/If-statement/if-statement-two.go
@@ -1,1 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// Format of "if/else" statement
+	x := 10
+	if x == 12 {
+		// statement
+		fmt.Println("case 1")
+	} else if x == 10 {
+		// statement
+		fmt.Println("case 2")
+	} else {
+		// statement
+		fmt.Println("None")
+	}
+}
 
