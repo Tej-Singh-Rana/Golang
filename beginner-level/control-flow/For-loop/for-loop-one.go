41e919b7ebad8ae1b73821174549d693fdd7dc4a
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-24 23:24:24 +0530

    For loop with increment the number

--- beginner-level/control-flow/For-loop/for-loop-one.go
+++ beginner-level/control-flow/For-loop/for-loop-one.go
@@ -4,11 +4,26 @@ import (
 	"fmt"
 )
 
+var (
+	count int
+)
+
 func main() {
 	// Main func block
+	count = 20
+	for count := 20; count < 25; count++ {
+		fmt.Println(count)
+	}
 
 }
 
-
+/*
+__Output__:-
+20
+21
+22
+23
+24
+*/
 
 
