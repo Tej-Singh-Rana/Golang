012dd40fbdb3579f7d7f73abd677081b51ebfd23
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-25 23:57:35 +0530

    Keyword const

--- beginner-level/constants/constants-one.go
+++ beginner-level/constants/constants-one.go
@@ -4,7 +4,18 @@ import "fmt"
 
 /* We use "const" keyword for constants. Ex:- const Key = "900"
 Constants cannot be declared using the ":=" syntax. Like const Jago := "Alert"
-We can use boolean, numeric, string and character in place of values.
+We can use boolean, numeric, string and character in place of values. (keyword <variable-name> type = value)
+Ex:-
+const val bool = true
+const strong string = "Power"
+const speed int = 250
 */
 
-func main() {}
+const key int = 900
+
+func main() {
+	// In case, if you'll assign a new value again 1000 to "key".
+	//** cannot assign to key (declared const) **
+	// key = 1000
+	fmt.Println(key)
+}
