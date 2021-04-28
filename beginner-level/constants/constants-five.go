ba1d4aaf04046ac5e9526cfb1750bec4025d3f1a
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-28 23:55:48 +0530

    Constant with Functions

--- beginner-level/constants/constants-five.go
+++ beginner-level/constants/constants-five.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"fmt"
+)
+
+// Package level declaration
+// Short declaration(:=) only can use within that function block not an outside of that function. Ex:- foo := 20
+// Can say that it's auto detect "type" according to an assignment.
+const msg = "NoOne"
+
+func main() {
+	// cannot assign to msg (declared const)
+	// 	msg = "NoBody"
+
+	fmt.Println(msg)
+
+	rowdy()
+}
+
+func rowdy() {
+	// cannot assign to msg (declared const)
+	// msg = "EveryBody"
+	fmt.Println(msg)
+}
