42b374b7bcff4e7337ebf4cca79778b82dc9b449
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-04-29 23:55:45 +0530

    Create if-statement-four.go

---
+++ beginner-level/control-flow/If-statement/if-statement-four.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+var plot string
+var pinCode int
+
+func main() {
+	// Identify the write plot and pinCode
+	plot = "R-21"
+	pinCode = 320012
+
+	if plot == "R-21" {
+		fmt.Println("PINCODE =", pinCode)
+		fmt.Println("You're in correct plot.")
+	} else {
+		fmt.Println("Enter correct information.")
+	}
+
+}
