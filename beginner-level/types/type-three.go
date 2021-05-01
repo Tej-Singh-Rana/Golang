773f4afb7a6bae79d1e1ae7c7e4971052f0356f3
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-05-01 23:37:47 +0530

    Update type-three.go

--- beginner-level/types/type-three.go
+++ beginner-level/types/type-three.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Declared new Type
+
+type Number int
+
+type Role string
 
 func main() {
+	var name Role = "SRE"
+	var position Number = 1
 
-	
+	fmt.Println(name, position)
+	fmt.Printf("Type = %T, %T", name, position)
 }
