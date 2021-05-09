7653f8052ef5a3b02e904dc16255898cfffd5bc7
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-05-09 23:46:54 +0530

    Update break.go

--- beginner-level/control-flow/Break&Continue/break.go
+++ beginner-level/control-flow/Break&Continue/break.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	for num1 := 1; num1 <= 10; num1++ {
+		if num1 == 5 {
+			fmt.Println("Matched, value =", num1)
+			// After getting the desired value, the loop will exit. It won't continue.
+			break
+		} else {
+			fmt.Println("Increased by 1 = ", num1)
+		}
+	}
+}
+
+
+
+
+/* 
+__Output__:-
+Increased by 1 =  1
+Increased by 1 =  2
+Increased by 1 =  3
+Increased by 1 =  4
+Matched, value = 5
+*/
