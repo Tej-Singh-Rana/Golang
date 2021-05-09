c3f1594a38900c3e5834dee841ac6a816f1d276e
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-05-09 23:44:47 +0530

    Update continue.go

--- beginner-level/control-flow/Break&Continue/continue.go
+++ beginner-level/control-flow/Break&Continue/continue.go
@@ -1,1 +1,29 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	for num1 := 5; num1 <= 10; num1++ {
+		if num1 == 6 {
+			fmt.Println("Matched, value =", num1)
+			// After getting the desired value, the loop won't exit. It'll continue till value 10.
+			continue
+		} else {
+			fmt.Println("Increased by 1 = ", num1)
+		}
+	}
+}
+
+
+
+/* 
+__Output__:-
+Increased by 1 =  5
+Matched, value = 6
+Increased by 1 =  7
+Increased by 1 =  8
+Increased by 1 =  9
+Increased by 1 =  10
+*/
