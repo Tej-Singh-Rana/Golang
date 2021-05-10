fb96a56c86ce589ec8b72b44794b6a1518c1378c
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-05-10 22:39:22 +0530

    multiple declaration from single VAR keyword

--- beginner-level/variables/variables-type-eighteen.go
+++ beginner-level/variables/variables-type-eighteen.go
@@ -1,1 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var (
+		path        = "Too long"
+		result      = "Don't wait for result"
+		goal        = "Learning new technologies"
+		presentYear = 2021
+	)
+
+	fmt.Println(path)
+	fmt.Println(result)
+	fmt.Println(goal)
+	fmt.Println(presentYear)
+	fmt.Println("")
+}
 
