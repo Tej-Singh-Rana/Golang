3d314ae8bb56a59178e566394bf2447a2856d41e
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-06-20 23:55:17 +0530

    If/else/if statement

--- beginner-level/control-flow/If-statement/if-statement-three.go
+++ beginner-level/control-flow/If-statement/if-statement-three.go
@@ -1,1 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// "IF" statement
+	var fruit string
+
+	fmt.Println("Enter the fruit name, It will print the month name\n 1. Orange\n 2. Mango\n 3. Banana\n 4. Papaya\n 5. Guava\n")
+	fmt.Scanln(&fruit)
+	if fruit == "Orange" {
+		fmt.Println("Month of September")
+	} else if fruit == "Mango" {
+		fmt.Println("Month of June")
+	} else if fruit == "Banana" {
+		fmt.Println("Month of March")
+	} else if fruit == "Papaya" {
+		fmt.Println("Month of August")
+	} else if fruit == "Guava" {
+		fmt.Println("Month of October")
+	} else {
+		fmt.Println("No fruits from the list")
+	}
+}
 
