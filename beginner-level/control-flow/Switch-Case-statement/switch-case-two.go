6c86613f42d3053caebfee7f9ad443fb714d7835
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-06-22 21:49:04 +0530

    switch case with IF statement

--- beginner-level/control-flow/Switch-Case-statement/switch-case-two.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-two.go
@@ -1,1 +1,37 @@
+package main
 
+import (
+	"fmt"
+	c "runtime"
+)
+
+// var x int = 9
+// var y int = 10
+
+func main() {
+	// code block
+	// switch case
+	switch x := 9; x {
+	case 9:
+		fmt.Println("System Info...")
+		fmt.Println("OS type =", c.GOOS)
+		if c.GOOS == "windows" {
+			fmt.Println("Windows 10 Pro")
+		} else if c.GOOS == "linux" {
+			fmt.Println("Linux")
+		}
+	case 10:
+		fmt.Println("Linux")
+	default:
+		fmt.Println("Not Found")
+
+	}
+}
+
+
+/*
+Output :-
+System Info...
+OS type = linux
+Linux
+*/
