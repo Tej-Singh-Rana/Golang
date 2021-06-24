832dbb73cb48018b7770434ddd486c7e07285bf2
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-06-24 23:59:24 +0530

    If/Else/If statement

--- beginner-level/control-flow/If-statement/if-statement-one.go
+++ beginner-level/control-flow/If-statement/if-statement-one.go
@@ -1,10 +1,42 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
+// Main entry point
+
 func main() {
-  // func body
+	// Main func body
+	var token int
+	fmt.Scanln(&token)
+	var name string
+	fmt.Scanln(&name)
+
+	if token == 1 && name == "ram" {
+		token = 1 * 5
+		fmt.Printf("Token number is %v", token)
+	} else if token == 2 && name == "ali" {
+		token = 2 * 5
+		fmt.Printf("Token number is %v", token)
+	} else if token == 3 && name == "john" {
+		token = 3 * 5
+		fmt.Printf("Token number is %v", token)
+	} else if token == 4 && name == "hari" {
+		token = 4 * 5
+		fmt.Printf("Token number is %v", token)
+	} else if token == 5 && name == "tej" {
+		token = 5 * 5
+		fmt.Printf("Token number is %v", token)
+	} else {
+		fmt.Println("Not found.")
+	}
 
 }
+
+/*
+Output :-
+
+Token number is 25
+
+*/
