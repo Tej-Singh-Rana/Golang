31a9bfc1a46c9e77abd9042d8e88d132ab096cea
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-07-01 23:51:21 +0530

    If statement

--- beginner-level/control-flow/If-statement/if-statement-eight.go
+++ beginner-level/control-flow/If-statement/if-statement-eight.go
@@ -1,1 +1,6 @@
+package main
 
+func main() {
+	// Main code block
+
+}
--- beginner-level/control-flow/If-statement/if-statement-five.go
+++ beginner-level/control-flow/If-statement/if-statement-five.go
@@ -1,1 +1,18 @@
+package main
 
+import "fmt"
+
+func main() {
+	// main code block
+
+	book := "Himalayan Travels"
+	var prize int
+	fmt.Println("Enter the amount to purchase the book :- ")
+	fmt.Scanln(&prize)
+
+	if prize < 300 {
+		fmt.Println("Added into the cart book:", book)
+	} else {
+		fmt.Printf("Amount is not sufficient to purchase %s.", book)
+	}
+}
