e09f08d452679d0976b918d5d0fba5e55ddc776e
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-07-09 00:04:04 +0530

    first array [NULL]

---
+++ beginner-level/arrays/array-one.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// main code block
+	// If it's not holding any value then it would be zero value.
+	var arr []int
+
+	fmt.Println("Empty value:", arr)
+}
+
+/*
+_Output_:-
+Empty value: []
+*/
