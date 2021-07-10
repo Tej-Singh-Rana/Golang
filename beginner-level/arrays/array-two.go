783357f2af21f00df52102f1a3b4a856e80e837f
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-07-10 23:31:48 +0530

    Update array-two.go

--- beginner-level/arrays/array-two.go
+++ beginner-level/arrays/array-two.go
@@ -1,1 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Main code block
+
+	var arr = [3]string{}
+
+	arr[0] = "numeric"
+	arr[1] = "alpha"
+	arr[2] = "boolean"
+
+	fmt.Println(arr[0], arr[1], arr[2])
+
+}
+
+/*
+_Output_:-
+numeric alpha boolean
+
+*/
 
