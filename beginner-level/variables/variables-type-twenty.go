65deef2103cd2f1772a3cf4d8d0f41ecda83d39d
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-07-16 00:08:27 +0530

    "within a program changes its type and values"

--- beginner-level/variables/variables-type-twenty.go
+++ beginner-level/variables/variables-type-twenty.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// Declared multiple values
 var (
 	exprOne string
 	exprTwo int
@@ -11,5 +12,34 @@ var (
 )
 
 func main() {
+	// Main code block
+	//Initialized values
+	exprOne = "John"
+	exprTwo = 2021
+	status = false
+
+	fmt.Printf("%s details on %d and it's condition %t.", exprOne, exprTwo, status)
+
+	// Changed the type
+	var (
+		exprOne int
+		exprTwo string
+		status  bool
+	)
+
+	//Initialized values with different type.
+	exprOne = 2021
+	exprTwo = "Rocky"
+	status = true
+
+	fmt.Printf("\n%s details on %v and it's condition %t.", exprTwo, exprOne, status)
 
 }
+
+
+/*
+_Output_:-
+John details on 2021 and it's condition false.
+Rocky details on 2021 and it's condition true.
+
+*/
