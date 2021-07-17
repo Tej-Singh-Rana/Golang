1b114b2882fb6322fcfd822eb47c162e7061db65
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-07-17 23:41:03 +0530

    slice literal with for loop && range func

--- beginner-level/slices/slices-three.go
+++ beginner-level/slices/slices-three.go
@@ -1,1 +1,34 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+   // Main code block
+  // Slice literal, where we don't specify length inside square brackets[]. 
+
+	var student = []string{
+		"john",
+		"jacob",
+		"rahul",
+		"raghu",
+		"narayan",
+	}
+
+	// for loop with range function
+	for _, count := range student {
+		fmt.Println(count)
+	}
+}
+
+
+/*
+_Output_:-
+john
+jacob
+rahul
+raghu
+narayan
+
+*/
