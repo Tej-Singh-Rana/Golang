4d8a1ce36aeddf7c9c6f4c75eab30ebe1ad8f471
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-07-17 23:57:51 +0530

    example of an array with index [start:end]

--- beginner-level/arrays/array-five.go
+++ beginner-level/arrays/array-five.go
@@ -1,1 +1,45 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	// Main code block
+	// a slice of type string
+  // create a slice from an array, specified size
+	var student = [5]string{
+		"john",
+		"jacob",
+		"rahul",
+		"raghu",
+		"narayan",
+	}
+	x := student[0:3]
+	y := student[0:]
+	z := student[2:4]
+	final := z[2:3]
+
+	fmt.Println("######### x ########")
+	fmt.Println(x)
+	fmt.Println("######### y ########")
+	fmt.Println(y)
+	fmt.Println("######### z ########")
+	fmt.Println(z)
+	fmt.Println("######### final ########")
+	fmt.Println(final)
+}
+
+
+/*
+_Output_:-
+######### x ########
+[john jacob rahul]
+######### y ########
+[john jacob rahul raghu narayan]
+######### z ########
+[rahul raghu]
+######### final ########
+[narayan]
+
+*/
