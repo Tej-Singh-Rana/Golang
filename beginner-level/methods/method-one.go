f0c5081875069a231fbdb2fd767fd8b2b54440a7
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-07-18 23:30:39 +0530

    Update method-one.go

--- beginner-level/methods/method-one.go
+++ beginner-level/methods/method-one.go
@@ -1,1 +1,27 @@
+package main
 
+import (
+	"fmt"
+)
+
+// Declared own struct type
+type Bus struct {
+	route string
+	name  string
+}
+
+// Methods
+func (b Bus) info() string {
+	return "Name of the bus is " + b.name + " and route of that bus is " + b.route +"."
+}
+
+func main() {
+	raw := Bus{name: "Plaza", route: "16"}
+	fmt.Println(raw.info())
+}
+
+/*
+_Output_:-
+Name of the bus is Plaza and route of that bus is 16.
+
+*/
