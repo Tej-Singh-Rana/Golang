c4f7652992673616fb5847ca50a45e2a0df2a193
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-07-19 21:58:10 +0530

    Match the value directly to the case

--- beginner-level/control-flow/Switch-Case-statement/switch-case-four.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-four.go
@@ -7,6 +7,26 @@ import (
 
 func main() {
 	// main func body
-	
+	// switch case statement
+	var (
+		mustang string = "red"
+	)
+	// Declared and initialized earlier. Now, we will match the value directly to the case.
+	switch {
+	case mustang == "black":
+		fmt.Println("Color not matched!!")
+
+	case mustang == "red":
+		fmt.Println("Color matched and that's", mustang, ".")
+
+	default:
+		fmt.Println("Not Found!!")
+	}
 
 }
+
+/*
+_Output_:-
+Color matched and that's red .
+
+*/
