e5be9646e3edf58560a5e833a4174b1d09e0340c
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-07-19 21:56:59 +0530

    Added new comment for more clarification

--- beginner-level/control-flow/Switch-Case-statement/switch-case-one.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-one.go
@@ -9,7 +9,9 @@ func main() {
 	// main func body
 	// Simple switch case statement
 	var name = "John"
+	// changed the value
 	name = "Ram"
+	// So var "name" value will match to each case. If it's true then it will not move to the next line, even if second line is true.
 	switch name {
 	case "Ram":
 		fmt.Println("This is Ram.")
