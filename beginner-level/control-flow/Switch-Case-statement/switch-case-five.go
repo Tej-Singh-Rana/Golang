af016a5aa53b966b22fb6c7e4bdb208f99d9ae6d
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-07-19 22:07:43 +0530

    Use case of Duplicate and multiple values

--- beginner-level/control-flow/Switch-Case-statement/switch-case-five.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-five.go
@@ -4,7 +4,36 @@ import (
 	"fmt"
 )
 
+var (
+	factor int = 5
+)
+
 func main() {
-	// Main block
+	// Main code block
+	// If value matched then it won't move to the next line, even that's resulting to true.
+	switch factor {
+
+	// Assign it multiple values to match.
+	case 1, 2, 3:
+		fmt.Println("1, 2, 3")
+
+	case 4, 5:
+		fmt.Println("4 and 5")
+
+	// You can only have one case statement for each value.
+	// If we will uncomment this, It will give an error.
+	// case 5:
+	//	fmt.Println("Perfect!!")
+
+	default:
+		fmt.Println("Recheck the number.")
+
+	}
 
 }
+
+/*
+_Output_:-
+4 and 5
+
+*/
