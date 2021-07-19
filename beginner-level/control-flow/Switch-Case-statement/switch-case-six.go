2b5804df47dd8b1b4a1b8d7cb49df6eae64d932a
tej-singh-rana <iamtejsinghrana@gmail.com>
2021-07-19 21:58:49 +0530

    default case

--- beginner-level/control-flow/Switch-Case-statement/switch-case-six.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-six.go
@@ -5,8 +5,27 @@ import (
 	"fmt"
 )
 
+var examYear int = 2017
+
 func main() {
 	// main func body
-	
+
+	// "default:" case used when no case matches.
+	switch {
+	case examYear == 2018:
+		fmt.Println("Exam year was 2018.")
+	case examYear == 2020:
+		fmt.Println("Exam year was 2020.")
+
+		// No matches found so default should be printed.
+	default:
+		fmt.Println("None of the year match.")
+	}
 
 }
+
+/*
+_Output_:-
+None of the year match.
+
+*/
