7905ffd7fe2b28fc86f3494d98859bee77331986
Tej Singh Rana <iamtejsinghrana@gmail.com>
2021-08-06 00:00:01 +0530

    If/else/if statement

--- beginner-level/control-flow/If-statement/if-statement-six.go
+++ beginner-level/control-flow/If-statement/if-statement-six.go
@@ -4,5 +4,18 @@ import "fmt"
 
 func main() {
 	// main code block
-  
+	fmt.Println("Enter two values")
+  var num1 int
+	var num2 int
+	fmt.Scanln(&num1)
+	fmt.Scanln(&num2)
+	if num1 == num2 {
+		fmt.Println("Both are same.")
+	} else if num1 < 10 {
+		fmt.Println("Number 1 is below 10.")
+	} else if num2 < 10 {
+		fmt.Println("Number 2 is below 10.")
+	} else {
+		fmt.Println("It's out of the number.")
+	}
 }
