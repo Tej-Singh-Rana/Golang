3b2a5c6e79879670a06d17fa136a8cf5dbdcd2e3
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-08-12 23:59:51 +0530

    Update pointers-two.go

--- beginner-level/pointers/pointers-two.go
+++ beginner-level/pointers/pointers-two.go
@@ -6,6 +6,19 @@ import (
 
 func main() {
 	// Main code block
+       // type *int is a pointer to a "x" value
+	var x *int
+
+	y := 12
+	// Address the value
+	x = &y
+    
+	fmt.Println(*x)
 
 }
 
+/*
+_Output_:-
+12
+
+*/
