ef727567aba113bf96eaf9c21d429fd133932708
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-08-16 00:45:43 +0530

    Assign it function literal to a variable

--- beginner-level/functions/func-six.go
+++ beginner-level/functions/func-six.go
@@ -5,6 +5,23 @@ import (
 )
 
 func main() {
- // Code block
-  
+	// Main code block
+
+	// Anonymous Function
+	// We can assign it to the variable
+	number := func() {
+		var x int = 12345
+		fmt.Println(x)
+	}
+	
+	// Now, you can call this variable as a func
+	number()
 }
+
+
+/*
+_Output_:-
+12345
+
+
+*/
