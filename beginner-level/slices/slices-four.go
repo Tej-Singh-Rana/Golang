bf5a2fb63b8e390550b61fac1c078db4d0dbc138
Tej Singh Rana <iamtejsinghrana@gmail.com>
2021-08-20 00:58:39 +0530

    In slices, element type should be same

--- beginner-level/slices/slices-four.go
+++ beginner-level/slices/slices-four.go
@@ -1,1 +1,35 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	// Main code block
+
+	// Every element in the slices should be of the same type.
+	// In below example, declared "int" value. Because of that we are getting an error.
+	// Error :- cannot use 2403 (type untyped int) as type string in slice literal
+
+	// var x []string = []string{"world", "famous", "place", 2403, "is", "jaipur"}
+
+	// Trying after removing the integer.
+
+	var x []string = []string{"World", "famous", "city", "is", "Jaipur."}
+	for _, i := range x {
+
+		fmt.Println(i)
+
+	}
+
+}
+
+/*
+_Output_:-
+World
+famous
+city
+is
+Jaipur.
+
+*/
