ad7af380805be4d6d59a1950882a5ed08e5fead1
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-08-23 23:59:07 +0530

    Slices

--- beginner-level/slices/slices-five.go
+++ beginner-level/slices/slices-five.go
@@ -6,13 +6,31 @@ import (
 
 func main() {
 	// Main code block
+	group := []string{"class1", "class2", "class3", "class4"}
+
+	for _, value := range group {
+
+		fmt.Println("List of classes - ", value)
+
+	}
+
+	// If we are trying to filter only "class1"
+
+	fmt.Println(group[0])
+	
+	// If filtering all items after "class2"
+	
+	fmt.Println(group[1:])
 
 }
 
-
 /*
 _Output_:-
-
-
+List of classes -  class1
+List of classes -  class2
+List of classes -  class3
+List of classes -  class4
+class1
+[class2 class3 class4]
 
 */
