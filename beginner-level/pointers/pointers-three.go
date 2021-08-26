9368dfe945a487049a2dae15e1bb9956743a27a9
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-08-26 23:44:05 +0530

    declare pointer

--- beginner-level/pointers/pointers-three.go
+++ beginner-level/pointers/pointers-three.go
@@ -6,5 +6,20 @@ import (
 
 func main() {
 	// Main code block
-  
+        
+	gradeA := "pass"
+	
+	// Declares pointer 
+	var gradeB *string
+
+	gradeB = &gradeA
+	fmt.Println(&gradeA, &gradeB)
+
 }
+
+
+/*
+_Output_:-
+0xc00009e210 0xc0000b2018
+
+*/
