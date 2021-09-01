a7860e3ee1dcfda5347b8b0004ea611b1bb1fde2
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-01 23:59:38 +0530

    find the special character

--- beginner-level/control-flow/Switch-Case-statement/switch-case-seven.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-seven.go
@@ -7,6 +7,28 @@ import (
 
 func main() {
 	// main func body
-	
+	// switch case statement
+
+	foo := '*'
+
+	// Match the given value
+	switch foo {
+	case 0, 1, 2, 3, 4, 5:
+		fmt.Println("Numeric case")
+	case '*', ')', '(', '&', '+', '-':
+		fmt.Println("Special case")
+	default:
+		fmt.Println("Not able to detect")
+
+	}
 
 }
+
+
+/*
+_Output_:-
+Special case
+
+
+
+*/
