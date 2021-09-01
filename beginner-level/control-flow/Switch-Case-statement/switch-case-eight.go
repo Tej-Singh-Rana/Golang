fec9a0b09046cd56520bbbf95b6f97bd6e5bfab8
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-02 00:45:04 +0530

    switch case with if/else/if and fallthrough

--- beginner-level/control-flow/Switch-Case-statement/switch-case-eight.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-eight.go
@@ -3,6 +3,34 @@ package main
 import "fmt"
 
 func main() {
- // code block
-  
+	// Main code block
+	logStart := "success"
+	// logStart = "fail"
+	
+	switch logStart {
+
+	case logStart:
+		if logStart == "error" || logStart == "fail" {
+			fmt.Println("Found an error!!")
+		} else if logStart == "success" || logStart == "pass" {
+			fmt.Println("Process passed!!")
+		} else {
+			fmt.Println("Uknown bug.")
+		}
+		// It will pass to the next case even if case 1 is successfully executed.
+		fallthrough
+
+	case logStart:
+		fmt.Println("Printing the value of logStart :", logStart)
+
+	}
+
 }
+
+/*
+_Output_:-
+Process passed!!
+Printing the value of logStart : success
+
+
+*/
