bb9fc6a48654bbad267b390e66e4441f0b84d0ac
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-04 00:11:48 +0530

    Execution order in Switch case

--- beginner-level/control-flow/Switch-Case-statement/switch-case-ten.go
+++ beginner-level/control-flow/Switch-Case-statement/switch-case-ten.go
@@ -1,12 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// This one will be evaluated first.
+func Vision(x int) int { fmt.Println(x); fmt.Println("Looping"); return x }
 
 func main() {
-        // Main func block
-	// code block
+	// Main code block
 
+	switch Vision(10) {
 
+	// Evaluated left to right and top to bottom.
+	// Each value will be gone through the Vision() func first. As you can see from the Output result.
+	case Vision(5), Vision(10), Vision(15):
+		fmt.Println("Final End") // --> At the end, it will print "Final End".
+
+	case Vision(20):
+		fmt.Println("Final Chapter")
+	}
 
 }
 
+/*
+_Output_:-
+10
+Looping
+5
+Looping
+10
+Looping
+Final End
+
+*/
