427bd3462c67a37fbdca71f1a25cd9ce10fa3cad
Tej Singh Rana <iamtejsinghrana@gmail.com>
2021-09-08 12:29:14 +0530

    Type foo struct

--- beginner-level/types/type-one.go
+++ beginner-level/types/type-one.go
@@ -2,7 +2,35 @@ package main
 
 import "fmt"
 
+// type struct
+type foo struct {
+	Name   string
+	Class  string
+	Number int64
+}
+
 func main() {
 	// Main function body
 
+	call := foo{
+		Name:   "Rahul",
+		Class:  "First",
+		Number: 12,
+	}
+
+	fmt.Println(call)
+	// Printing individual values
+	fmt.Println(call.Name)
+	fmt.Println(call.Class)
+	fmt.Println(call.Number)
+
 }
+
+/*
+_Output_:-
+{Rahul First 12}
+Rahul
+First
+12
+
+*/
