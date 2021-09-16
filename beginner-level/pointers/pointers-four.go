36e8ccb642172947264dce0914971af508792f43
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-16 23:38:06 +0530

    integer pointer (*int)

--- beginner-level/pointers/pointers-four.go
+++ beginner-level/pointers/pointers-four.go
@@ -1,4 +1,29 @@
+// https://play.golang.org/p/lyEohUKW4pK
+
 package main
 
+import (
+	"fmt"
+)
 
-func main() {}
+func main() {
+	// Main code block
+
+	// integer pointer
+	var cash *int
+
+	widCash := 5000
+	cash = &widCash
+
+	fmt.Println(widCash, cash, *cash)
+
+	// It's type
+	fmt.Printf("cash type is: %T, widCash type is: %T", cash, widCash)
+}
+
+/*
+_Output_:-
+5000 0xc000112000 5000
+cash type is: *int, widCash type is: int
+
+*/
