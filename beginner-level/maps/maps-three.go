0ec2281f16bab4ea60bb6d3227c650762b025995
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-20 23:36:40 +0530

    Maps with make function

--- beginner-level/maps/maps-three.go
+++ beginner-level/maps/maps-three.go
@@ -1,9 +1,39 @@
+// https://play.golang.org/p/heUxweIrbQq
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// Declare and initialize, type map[string]string
+var tempClass map[string]string
 
 func main() {
-    // Main code block
-  
+	// Main code block
+	// maps with make function
+	tempClass = make(map[string]string)
 
+	tempClass["first"] = "High"
+	tempClass["second"] = "Medium"
+	tempClass["third"] = "Average"
+	tempClass["four"] = "Low"
+
+	// Type and Value
+	fmt.Printf("Type of variable = %T, key-value pairs = %v\n", tempClass, tempClass)
+
+	// underScore(_) will ignore that index value and will not be used in the std output.
+	for _, value := range tempClass {
+		fmt.Println("tempClass values = ", value)
+	}
 }
+
+/*
+_Output_:-
+Type of variable = map[string]string, key-value pairs = map[first:High four:Low second:Medium third:Average]
+tempClass values =  High
+tempClass values =  Medium
+tempClass values =  Average
+tempClass values =  Low
+
+*/
