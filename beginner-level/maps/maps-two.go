3c5723115e1a1694768b92ac63acd1b74193981e
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-20 23:44:32 +0530

    Can't add key/value to the nil map

--- beginner-level/maps/maps-two.go
+++ beginner-level/maps/maps-two.go
@@ -1,9 +1,29 @@
+// https://play.golang.org/p/YBVSJtuEQKl
+
 package main
 
 import "fmt"
 
+var m map[string]string
 
 func main() {
-    // Main code block
+	// Main code block
+	// Map value is nil, as you can see in the output.
+	fmt.Println(m)
+
+	// Adding values
+	// We can't assign key/value in the nil map
+	m["test"] = "Fail"
+	fmt.Println(m)
 
 }
+
+/*
+_Output_:-
+map[]
+panic: assignment to entry in nil map
+
+goroutine 1 [running]:
+main.main()
+
+*/
