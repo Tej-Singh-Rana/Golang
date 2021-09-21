6259ca37fad3fe459dc570654946ec729b2ad883
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-21 23:55:00 +0530

    Fetching values individually

--- beginner-level/structs/structs-two.go
+++ beginner-level/structs/structs-two.go
@@ -1,17 +1,33 @@
+// https://play.golang.org/p/rSGxjKu4Xre
+
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Class struct {
+	name  string
+	email string
+}
 
 func main() {
-     // Main code block
 
+	user := Class{"Rock", "rock@sample.com"}
 
+	// %+v gives more details
+	fmt.Printf("Lists of user: %+v\n", user)
+
+	// Fetching individual values
+	fmt.Println("User name: ", user.name)
+	fmt.Println("Email address: ", user.email)
 }
 
 /*
 _Output_:-
-
+Lists of user: {name:Rock email:rock@sample.com}
+User name:  Rock
+Email address:  rock@sample.com
 
 
 */
-
