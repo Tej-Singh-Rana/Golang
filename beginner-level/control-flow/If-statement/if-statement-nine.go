f084a80f259e37fc96d54461c1c060b4972f958e
Tej Singh Rana <iamtejsinghrana@gmail.com>
2021-09-22 23:54:07 +0530

    If/Else statement basics

--- beginner-level/control-flow/If-statement/if-statement-nine.go
+++ beginner-level/control-flow/If-statement/if-statement-nine.go
@@ -1,6 +1,19 @@
 package main
 
 func main() {
-	// main code block
+	// Main code block
+
+	if x := 10; x > 20 {
+		print("Lower value")
+	} else {
+		print("Higher value")
+	}
 
 }
+
+/*
+_Output_:-
+Higher value
+
+
+*/
