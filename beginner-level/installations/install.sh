d97494a90cab00e8de306576e33f292ab9e2827b
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-23 00:05:54 +0530

    Go installation steps

--- beginner-level/installations/install.sh
+++ beginner-level/installations/install.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+wget https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
+
+tar -xvf go1.*
+
+cp -rv go /usr/bin/
+
+echo "export PATH=$PATH:/usr/bin/go/bin" >> ~/.bashrc
+
+source ~/.bashrc
+
+go version
