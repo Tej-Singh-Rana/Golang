b6a1270d49ccebdf33a6d24fa94e45717e4c2abe
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-25 23:59:11 +0530

    empty interface{}

--- beginner-level/interfaces/interfaces-one.go
+++ beginner-level/interfaces/interfaces-one.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+// empty interface
+
+type First interface{}
+
 func main() {
-    // Main code block
+	// Main code block
+
+	var t First
+
+	fmt.Println("Value of empty interface", t)
+
 }
+
+/*
+_Output_:-
+Value of empty interface <nil>
+
+
+*/
