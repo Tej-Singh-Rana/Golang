a39ea2d7f9c6b952bb70f335d54598d8f8db4c05
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-26 23:37:02 +0530

    variadic function

--- beginner-level/functions/func-nine.go
+++ beginner-level/functions/func-nine.go
@@ -1,8 +1,43 @@
+// https://play.golang.org/p/dvU8LdQmGcg
+
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-   // Main body block
+	// Main code block
 
-
+	passMulti := arbitFunc(1, 2, 3, 4, 5)
+	fmt.Println("Total number : ", passMulti)
 
 }
+
+// We can pass multiple inputs
+// three dot (...)
+
+func arbitFunc(numbers ...int) int {
+	number := 0
+
+	for _, num := range numbers {
+		number += num
+
+		fmt.Println(number)
+
+	}
+
+	return number
+
+}
+
+/*
+_Output_:-
+1
+3
+6
+10
+15
+Total number :  15
+
+*/
