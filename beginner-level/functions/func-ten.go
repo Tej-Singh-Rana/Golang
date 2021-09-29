95d75afa9ac7069d281aa4dee03b08d5134fa057
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-09-29 23:55:37 +0530

    Two func created within a program

--- beginner-level/functions/func-ten.go
+++ beginner-level/functions/func-ten.go
@@ -1,8 +1,28 @@
+// https://play.golang.org/p/HShQqpFVWKJ
+
 package main
 
 import "fmt"
 
 func main() {
-    // Main code block
+	// Main code block
+
+	CodeBlock("This is a testing message!!")
 
 }
+
+// Print message
+
+func CodeBlock(p string) string {
+
+	fmt.Println("A message you wrote:", p)
+
+	return p
+
+}
+
+/*
+_Output_:-
+A message you wrote: This is a testing message!!
+
+*/
