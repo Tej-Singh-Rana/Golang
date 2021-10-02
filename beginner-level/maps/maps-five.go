01cc0cb71d21364d1ca7641b553cccc293007dfe
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-10-02 23:56:36 +0530

    map []string with strings pkg

--- beginner-level/maps/maps-five.go
+++ beginner-level/maps/maps-five.go
@@ -1,11 +1,28 @@
-package main 
+// https://play.golang.org/p/nL7SYcgGam0
 
-import "fmt"
+package main
 
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-   // Main code block
-  
+	// Main code block
 
+	var Ground []string = []string{"latency", "axis", "longitude", "latitude"}
+
+	fmt.Println(Ground)
+
+	// strings package, delimiter used as comma
+
+	fmt.Println(strings.Join(Ground, ","))
 
 }
+
+/*
+_Output_:-
+[latency axis longitude latitude]
+latency,axis,longitude,latitude
+
+*/
