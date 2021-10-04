4bcfb95c2cd26ae148b27be2f1d2a7df2e5a68c9
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-10-04 23:55:19 +0530

    Create for-method.go

---
+++ beginner-level/all-in-one/for-method.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Main code block
+
+}
+
+// Custom method
+
+func Custom(x int) int {
+
+	// Custom method code block
+
+}
+
+/*
+_Output_:-
+
+
+*/
