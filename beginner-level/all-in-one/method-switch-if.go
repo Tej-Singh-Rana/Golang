1c0540d18d9f8dff436817569f16d27301840b5c
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-10-04 23:58:36 +0530

    Method + Switch case statement + If/Else statement

---
+++ beginner-level/all-in-one/method-switch-if.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Main code block
+
+}
+
+func Logic(raw string) string {
+	// Logic method code block
+
+	switch {
+
+	case :
+		if {
+		} else {
+		}
+	}
+
+}
+
+/*
+_Output:-
+
+*/
