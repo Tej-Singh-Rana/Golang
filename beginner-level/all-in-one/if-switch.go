bc04d9d7f62728dc83f288b968f4c2c65b12c06c
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-10-05 23:54:59 +0530

    if - switch case statement

--- beginner-level/all-in-one/if-switch.go
+++ beginner-level/all-in-one/if-switch.go
@@ -1,8 +1,31 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	// Main code block
+	var x string = "Yes"
+	x = "No"
 
+	if x == "Yes" || x == "No" {
 
+		switch x {
 
-	
+		default:
+			fmt.Println("Find out the correct!")
+
+		case "Yes":
+			fmt.Println("You are correct!")
+		}
+
+	}
 }
+
+/*
+_Output_:-
+Find out the correct!
+
+
+*/
