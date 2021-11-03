4fa81e6d2383c0018ea3805813e3dd8990d27873
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-11-03 23:47:31 +0530

    Calling another method in main method

--- beginner-level/functions/func-eleven.go
+++ beginner-level/functions/func-eleven.go
@@ -1,17 +1,22 @@
 package main
 
-import "fmt"
+func Caller() string {
 
-
-func main() {
-   // Main code block
-
+	return "Detected!!"
 
 }
 
+func main() {
+	// Main code block
+	// Assigned method to variable x
+	
+	x := Caller()
+	y := x
+	print(y)
+}
 
 /*
 _Output_:-
-
+Detected!!
 
 */
