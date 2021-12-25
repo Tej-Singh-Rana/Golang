974a15f889abee368578cfe3d1a1daccd2971e4f
Tej-Singh-Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2021-12-26 00:00:03 +0530

    total of sum + remainder with const var

--- beginner-level/constants/constants-ten.go
+++ beginner-level/constants/constants-ten.go
@@ -1,1 +1,17 @@
+package main
 
+import "fmt"
+
+func main() {
+	// Main code block
+  // total of sum and remainder
+	const x = 12 + 12
+	const y = 57 % 2
+	fmt.Println("Total of x and y =", x+y)
+}
+
+/*
+_Output_:-
+Total of x and y = 25
+
+*/
