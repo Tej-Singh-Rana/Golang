99ae434364b5504e6e2a4c5635660ab42ff287fa
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-02-05 23:49:39 +0530

    for loop with continue

--- beginner-level/control-flow/For-loop/for-loop-six.go
+++ beginner-level/control-flow/For-loop/for-loop-six.go
@@ -4,13 +4,28 @@ import "fmt"
 
 func main() {
 	// Main code block
-        // "for" loop with "continue" 
-	
+	// "for" loop with "continue"
+	for i := 0; i <= 10; i++ {
+		fmt.Println(i)
+		continue
+	}
+	fmt.Println("Task completed!!")
 
 }
 
 /*
 _Output_:-
-
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Task completed!!
 
 */
