f327509d42fa16b045d5adeea8405bfddf1c4c23
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-02-05 23:56:03 +0530

    for loop without condition

--- beginner-level/control-flow/For-loop/for-loop-ten.go
+++ beginner-level/control-flow/For-loop/for-loop-ten.go
@@ -1,11 +1,32 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	// Main code block
+	x := 0
+	value := 10
+	for {
+		if x == value {
+			break
+		}
+		fmt.Println(x)
+		x++
 
+	}
 }
 
+/*
+_Output_:-
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+*/
