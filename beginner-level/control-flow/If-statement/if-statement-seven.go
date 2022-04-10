e31131c0da44acaeaa4f084f40e35e265a218e1e
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-04-10 23:21:41 +0530

    New method + if/else condition

--- beginner-level/control-flow/If-statement/if-statement-seven.go
+++ beginner-level/control-flow/If-statement/if-statement-seven.go
@@ -2,13 +2,23 @@ package main
 
 import "fmt"
 
+func text() string {
+	x := "Hello! Please accept my request."
+	return x
+}
 func main() {
 	// Main code block
 
+	request := text()
+	if request == "Hello! Please accept my request." {
+		fmt.Println("Message matched.")
+	} else {
+		fmt.Println("Message not matched.")
+	}
 }
 
 /*
 _Output_:-
-
+Message matched.
 
 */
