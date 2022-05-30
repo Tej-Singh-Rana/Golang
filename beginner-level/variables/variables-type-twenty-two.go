2e91c8d3262d35f28929c496a30020a284777d2b
Tej Singh Rana <58101587+Tej-Singh-Rana@users.noreply.github.com>
2022-05-31 00:01:27 +0530

    used backtick to print multi-line strings

--- beginner-level/variables/variables-type-twenty-two.go
+++ beginner-level/variables/variables-type-twenty-two.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
-var token = ``
+// Multiline strings
+var token = `Gin
+Echo
+Fiber
+All these are Go web framework.`
 
 func main() {
 	// Main code block
-	// Use case of backtick(`)
+	// Use case of backtick(``)
 	fmt.Println(token)
 }
 
 /*
 _Output_:-
-
-
+Gin
+Echo
+Fiber
+All these are Go web framework.
 */
+
+
